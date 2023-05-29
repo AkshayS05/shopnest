0a906a1587bd7c5ea2ce6a8443389bbe343f3a4d
akshay <akshaysharma5432@gmail.com>
2023-05-29 16:24:59 -0400

    add the footer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { NavbarComponent } from './components/banner/shared/navbar/navbar.component';
+import { FooterComponent } from './components/banner/shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BannerComponent,
     NavbarComponent,
+    FooterComponent,
 
   ],
   imports: [
