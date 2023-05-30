a0b23183cfda8a9ae79ec9288f8280c49d9cf8cb
akshay <akshaysharma5432@gmail.com>
2023-05-30 18:23:24 -0400

    handle image size of the cards

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -12,6 +12,11 @@ export class CardComponent {
 // to get out the data from the component
 @Output() onCardSelect= new EventEmitter<Card>();
 
+//to show a limited amount of information
+trimText(subTitle:string){
+  return subTitle.substring(0, 75) +'...';
+}
+
 //if item is out of stock, disable that button of add to card
 get isDisabled(){
   return this.card.stock === 0;
